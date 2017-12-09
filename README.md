58e222f6a302c4038149e2009c6351a342b75256
upperstream <developer@upperstream.io>
2017-12-09 11:26:19 +0900

    Add support for macOS

--- README.md
+++ README.md
@@ -15,11 +15,12 @@ Please note that you need the root privilege to run the script in order to perfo
 Tools to be installed are:
 
 * [Micro][] editor
-* [EditorConfig C Core][]
+* [EditorConfig Core C][]
 
-At the time of writing this, the script only supports [OpenBSD][].
+At the time of writing this, the script only supports [macOS][] and [OpenBSD][].
 
-[EditorConfig C Core]: https://github.com/editorconfig/editorconfig-core-c
+[EditorConfig Core C]: https://github.com/editorconfig/editorconfig-core-c
+[macOS]: https://www.apple.com/lae/macos/high-sierra/ "macOS High Sierra - Apple"
 [Micro]: https://micro-editor.github.io/ "Micro - Home"
 [OpenBSD]: https://www.openbsd.org/ "OpenBSD"
 
@@ -35,7 +36,7 @@ Run the script with `-n` option does nothing but merely prints what will be done
 Execute `./dotfiles_link_files -H` to learn more options.
 
 
-## Licencing
+## Licensing
 
 Files in this project are provided under the [ISC License][].
 See [LICENSE.txt](LICENSE.txt) file for details.
--- dotfiles_install_tools.sh
+++ dotfiles_install_tools.sh
@@ -5,29 +5,55 @@
 
 set -x
 
+install_package() {
+	case `uname` in
+		Darwin)
+			brew install $@
+			;;
+		OpenBSD)
+			pkg_add $@
+			;;
+	esac
+}
+
+install_editorconfig() {
+	case `uname` in
+		Darwin)
+			brew install editorconfig
+			;;
+		OpenBSD)
+			for t in cmake pcre; do
+				has $t || install $t
+			done
+			ftp -o- https://github.com/editorconfig/editorconfig-core-c/archive/v0.12.1.tar.gz | tar -zxf - -C /tmp
+			(cd /tmp/editorconfig-core-c-0.12.1 && cmake . && make && doas make install) && rm -rf /tmp/editorconfig-core-c-0.12.1
+			;;
+	esac
+}
+
 install() {
-case `uname` in
-	OpenBSD)
-		pkg_add $@
-		;;
-esac
+	for t in $@; do
+		case $t in
+			editorconfig)
+				install_editorconfig
+				;;
+			*)
+				install_package $t
+				;;
+		esac
+	done
 }
 
 has() {
 	command -v $1 >/dev/null
 }
 
-if [ `id -u` -ne `id -u root` ]; then
-	echo "You need the root privilege to run this script." 1>&2
-	exit 1
+if [ `id -u` -ne 0 ]; then
+	echo "You may need the root privilege to run this script." 1>&2
 fi
 
 # Micro editor
 has micro || install micro
 
-# EditorConfig core library
-for t in cmake pcre; do
-	has $t || install $t
-done
-ftp -o- https://github.com/editorconfig/editorconfig-core-c/archive/v0.12.1.tar.gz | tar -zxf - -C /tmp
-(cd /tmp/editorconfig-core-c-0.12.1 && cmake . && make && doas make install) && rm -rf /tmp/editorconfig-core-c-0.12.1
+# EditorConfig Core C
+has editorconfig || install editorconfig
