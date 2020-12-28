bfd3c8d27c8cb2aabf92a8459231cf037a29ddc7
upperstream <developer@upperstream.io>
2020-12-28 23:22:33 +0000

    Update meghanada settings

--- sets/jdk/.emacs.d/init.d/jdk.el
+++ sets/jdk/.emacs.d/init.d/jdk.el
@@ -2,17 +2,24 @@
   :ensure t
   :pin melpa-stable)
 
-(use-package meghanada
-  :ensure t
-  :pin melpa-stable)
+(meghanada-update-server)
 
+(require 'meghanada)
 (add-hook 'java-mode-hook
-	  (lambda ()
-	    ;; meghanada-mode on
-	    (meghanada-mode t)
-	    (setq c-basic-offset 2)
-	    ;; use code format
-	    (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+          (lambda ()
+            ;; meghanada-mode on
+            (meghanada-mode t)
+            (flycheck-mode +1)
+            (setq c-basic-offset 2)
+            ;; use code format
+            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+(cond
+   ((eq system-type 'windows-nt)
+    (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
+    (setq meghanada-maven-path "mvn.cmd"))
+   (t
+    (setq meghanada-java-path "java")
+    (setq meghanada-maven-path "mvn")))
 
 (use-package auto-complete
   :ensure t
